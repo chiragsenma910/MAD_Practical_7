1d55846eb5f96df62a2d41670b9eabb0ac61aaf3
MAD_prac_1 <senmachiragkumar@gmail.com>
2023-09-22 12:39:09 +0530

    i

--- app/src/main/java/com/example/mad_prac7_106_5b6/MainActivity.kt
+++ app/src/main/java/com/example/mad_prac7_106_5b6/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.mad_prac7_106_5b6
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import androidx.core.view.WindowCompat
+import com.example.mad_prac7_106_5b6.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initVideoPlayer()
+        binding.btnSwitch.setOnClickListener{
+            Intent(this,YoutubeActivity::class.java).also{startActivity(it)}
+        }
+    }
+    private fun initVideoPlayer(){
+        val mediaController=MediaController(this)
+        val uri:Uri=Uri.parse("android.resource://"+ packageName+ "/" +R.raw.thestoryoflight)
+        binding.myVideoview.setMediaController(mediaController)
+        mediaController.setAnchorView(binding.myVideoview)
+        binding.myVideoview.setVideoURI(uri)
+        binding.myVideoview.requestFocus()
+        binding.myVideoview.start()
     }
 }
